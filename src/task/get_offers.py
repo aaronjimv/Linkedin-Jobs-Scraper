08d9f5db4d0b8289eb40fa0838480a149fca671f
Aaron Jimenez <aaronjimv@gmail.com>
2023-11-08 14:32:59 -0800

    get_offers.py edited

--- src/task/get_offers.py
+++ src/task/get_offers.py
@@ -11,18 +11,32 @@ def get_offers(skill):
     
     if(url.status_code == 200):
         print(f" -- DATA SCRAPPER -- ")
+        
         html = BeautifulSoup(url.text,'html.parser')
-        data_offers = html.find('ul',{'class':'jobs-search__results-list'})
+        data_offers = html.find(
+            'ul',
+            {'class':'jobs-search__results-list'}
+        )
         offers_list = data_offers.find_all('li')
         results = []
+
         for offer in offers_list:
-            title = offer.find('h3',{'class':'base-search-card__title'})
-            company = offer.find('span',{'class':'job-search-card__location'})
-            urloffer = offer.find('a',{'class':'base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]'})
+            title = offer.find(
+                'h3',
+                {'class':'base-search-card__title'}
+            )
+            company = offer.find(
+                'span',
+                {'class':'job-search-card__location'}
+            )
+            urloffer = offer.find(
+                'a',
+                {'class':'base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]'}
+            )
             offers_finded = {
                 'job':title.get_text().strip(),
                 'location':company.get_text().strip(),
-                'url': urloffer['href'].strip()
+                'url':urloffer['href'].strip()
             }
             results.append(offers_finded)
         return results
