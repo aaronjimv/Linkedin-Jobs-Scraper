50cc4a0a868ebdaa38fea9fb9fd2ee59832b3c5a
Aaron Jimenez <aaronjimv@gmail.com>
2023-11-08 11:16:20 -0800

    variables renamed

--- main.py
+++ main.py
@@ -9,27 +9,27 @@ def get_offers(skill):
     if(url.status_code == 200):
         print(f"EXTRACCIÓN DE DATA...")
         html = BeautifulSoup(url.text,'html.parser')
-        dataOfertas = html.find('ul',{'class':'jobs-search__results-list'})
-        listaOfertas = dataOfertas.find_all('li')
-        resultadoOfertas = []
-        for oferta in listaOfertas:
-            titulo = oferta.find('h3',{'class':'base-search-card__title'})
-            empresa = oferta.find('span',{'class':'job-search-card__location'})
-            urlOferta = oferta.find('a',{'class':'base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]'})
-            dicOferta = {
-                'puesto':titulo.get_text().strip(),
-                'ubicacion':empresa.get_text().strip(),
-                'url': urlOferta['href'].strip()
+        data_offers = html.find('ul',{'class':'jobs-search__results-list'})
+        offers_list = data_offers.find_all('li')
+        results = []
+        for offer in offers_list:
+            title = offer.find('h3',{'class':'base-search-card__title'})
+            company = offer.find('span',{'class':'job-search-card__location'})
+            urloffer = offer.find('a',{'class':'base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]'})
+            offers_finded = {
+                'job':title.get_text().strip(),
+                'location':company.get_text().strip(),
+                'url': urloffer['href'].strip()
             }
-            resultadoOfertas.append(dicOferta)
-        return resultadoOfertas
+            results.append(offers_finded)
+        return results
 		
     else:
         print("error " + str(url.status_code))
         
 
 if __name__ == '__main__':
-    skill = input('ingrese skill a buscar en linkedin : ')
+    skill = input('Enter skill to search on Linkedin : ')
     offers = get_offers(skill)
     for offer in offers:
         print("="*100)
