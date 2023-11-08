132cfc470ac2edce32ce54baf08dec38f903ad4e
Aaron Jimenez <aaronjimv@gmail.com>
2023-11-08 14:03:20 -0800

    setup.py added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='linkedin offers search',
+    version='1.0.0',
+    description='package to scrapping linkedin offers',
+    author='Aaron Jimenez'
+)
